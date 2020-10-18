fbea016fa0ba8bb01518af1dbba724390d469b01
charlesvg <charles.vangoidsenhoven@gmail.com>
2020-10-19 00:14:57 +0200

    Added hitman, working background/camera movement

--- src/scenes/game-scene.ts
+++ src/scenes/game-scene.ts
@@ -1,11 +1,14 @@
 import * as Phaser from 'phaser';
 import TileSprite = Phaser.GameObjects.TileSprite;
 import Text = Phaser.GameObjects.Text;
+import CursorKeys = Phaser.Types.Input.Keyboard.CursorKeys;
+import Image = Phaser.Physics.Arcade.Image;
 
 export class GameScene extends Phaser.Scene {
-  private sprite: any;
+  private sprite: Image;
   private text: Text;
   private land: TileSprite;
+  private cursors: CursorKeys;
   constructor () {
     super({ key: 'GameScene' })
   }
@@ -21,22 +24,13 @@ export class GameScene extends Phaser.Scene {
 
   public create () {
 
-    let graphics = this.add.graphics()
-    graphics.setVisible(false)
-    graphics.fillStyle(0x00ff00, 1)
-    graphics.beginPath()
-    graphics.moveTo(0, 0)
-    graphics.lineTo(60, 20)
-    graphics.lineTo(0, 40)
-    graphics.lineTo(0, 0)
-    graphics.fillPath()
-    graphics.generateTexture('sprite', 60, 40)
-    this.sprite = this.add.sprite(400, 300, 'sprite').setOrigin(0.5)
+    this.sprite = this.physics.add.image(400, 300, 'hitman');
+    this.sprite.setOrigin(0.5,0.5);
 
     this.text = this.add.text(10, 10, 'angle');
 
     this.input.on('pointermove', (pointer: any)  => {
-      const angle = Phaser.Math.RAD_TO_DEG * Phaser.Math.Angle.Between(this.sprite.x, this.sprite.y, pointer.x, pointer.y)
+      const angle = Phaser.Math.RAD_TO_DEG * Phaser.Math.Angle.Between(400, 300, pointer.x, pointer.y)
       this.sprite.setAngle(angle)
       this.text.text = 'angle: ' + angle
       this.text.setColor('#ffffff');
@@ -47,14 +41,41 @@ export class GameScene extends Phaser.Scene {
     //  Resize our game world to be a 2000 x 2000 square
     // this.physics.world.bounds.setTo(-1000, -1000, 2000, 2000);
 
+    this.cursors = this.input.keyboard.createCursorKeys();
 
     //  Our tiled scrolling background
     this.land = this.add.tileSprite(800 / 2 , 600 / 2 , 800, 600, 'concrete');
 
     this.land.tilePositionX = -this.cameras.main.x;
     this.land.tilePositionY = -this.cameras.main.y;
+
+    // Z axis
     this.land.depth = -1;
 
+    this.cameras.main.startFollow(this.sprite, true);
+
     // https://phaser.io/examples/v3/view/camera/follow-sprite-small-bounds
+  }
+
+  public update() {
+    this.sprite.setVelocity(0);
+    var cam = this.cameras.main;
+    if (this.cursors.left.isDown)
+    {
+      this.sprite.setVelocityX(-400);
+    }
+    else if (this.cursors.right.isDown)
+    {
+      this.sprite.setVelocityX(400);
+    }
+
+    if (this.cursors.up.isDown)
+    {
+      this.sprite.setVelocityY(-400);
+    }
+    else if (this.cursors.down.isDown)
+    {
+      this.sprite.setVelocityY(400);
+    }
   }
 }
